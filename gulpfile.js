c4a443041b9aad466faac206f8f14819c8bcfc26
Ben Naylor <ben.naylor@fronde.com>
2015-03-16 19:30:04 +1300

    With some luck with have some herokus to play with

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,28 @@
 require('require-dir')('build/tasks');
+
+var gulp = require('gulp');
+
+gulp.task('serveprod', ['build'], function() {
+    var express = require('express');
+    var app = express();
+    app.use(express.static(__dirname));
+    app.set('port', (process.env.PORT || 5000));
+    
+    app.get('/', function(request, response) {
+        response.sendFile(__dirname + '/index.html');
+
+    });
+
+    app.listen(app.get('port'), function() {
+        console.log('Up and running on all cylinders');
+        console.log(' _________');
+        console.log('< moooooo >');
+        console.log(' ---------');
+        console.log('        \\   ^__^');
+        console.log('         \\  (oo)\\_______');
+        console.log('            (__)\\       )\\/\\');
+        console.log('                ||----w |');
+        console.log('                ||     ||');
+    });
+
+});
