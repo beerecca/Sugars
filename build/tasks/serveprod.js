e966c0e0d58e70a6916cb5a97e814acc63052f9a
Ben Naylor <ben.naylor@fronde.com>
2015-04-07 22:56:43 +1200

    fixing that jspm breaking all the things thing

--- build/tasks/serveprod.js
+++ build/tasks/serveprod.js
@@ -4,7 +4,7 @@ var runSequence = require('run-sequence');
 var gls = require('gulp-live-server');
 var paths = require('../paths');
 
-gulp.task('jspmgithub', shell.task('jspm config endpoints.github.auth ' + process.env.JSPM_GITHUB_AUTH));
+gulp.task('jspmgithub', shell.task('jspm config registry.github.auth ' + process.env.JSPM_GITHUB_AUTH));
 
 gulp.task('jspmdependencies', shell.task('jspm install'));
 
