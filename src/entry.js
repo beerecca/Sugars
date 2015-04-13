31ea492d5ff949ee9554bbcd4fd3aa744c6f6de8
Ben Naylor <ben.naylor@fronde.com>
2015-04-13 21:04:48 +1200

    classes on all the things

--- src/entry.js
+++ src/entry.js
@@ -9,96 +9,51 @@ export class Entry{
   constructor(http){
     this.heading = 'New Entry';
     this.glucose = 0;
-    this.quantity = 0;
     this.exercise = 0;
-    this.defaultFood = {
-      id : 4,
-      name : 'Select food item:',
-      unit : 'unit',
-      defaultAmount : 0,
-      carbs : 0,
-      quantity : 0
-    };
-    this.chosenFood = [
-      {
-        id : 4,
-        name : 'Select food item:',
-        unit : 'unit',
-        defaultAmount : 0,
-        carbs : 0,
-        quantity : 0
-      }];
-
-    this.food = [];
-    //this.http = http;
-
+   
+    //start the page with one empty entry food item
+    this.entryFoodItems = [ new EntryFoodItem() ];
+    
+    //initalize http client for ajaxy requests
     this.client = new HttpClient()
       .configure(x => {
         x.withBaseUri('http://sugars.herokuapp.com/api');
         x.withHeader('Content-Type', 'application/json');
     });
-    // this.food = [
-    //   {
-    //     id : 4,
-    //     name : 'Select food item:',
-    //     unit : 'unit',
-    //     defaultAmount : 0,
-    //     carbs : 0,
-    //     quantity : 0
-    //   },
-    //   {
-    //     id : 1,
-    //     name : 'Pizza',
-    //     unit : 'slice',
-    //     defaultAmount : 1,
-    //     carbs : 30,
-    //     quantity : 0
-    //   },
-    //   {
-    //     id : 2,
-    //     name : 'Rice',
-    //     unit : 'cup',
-    //     defaultAmount : 1,
-    //     carbs : 45,
-    //     quantity : 0
-    //   }, 
-    //   {
-    //     id : 3,
-    //     name : 'Bread',
-    //     unit : 'slice',
-    //     defaultAmount : 1,
-    //     carbs : 20,
-    //     quantity : 0
-    //   }
-    // ];
-  }
-
-  activate(){
-    return this.client.get('/food').then(response => {
+    
+    this.client.get('/food').then(response => {
       this.food = response.content;
-      this.food.unshift(this.defaultFood);
-       console.log('this.food plus default', this.food);
+      this.food.unshift({
+        id : null,
+        name : 'Select food item:',
+        unit : 'unit',
+        carbs : 0,
+        quantity : 0
+      });
     });
   }
 
-  addFood(){
-    this.chosenFood.push(this.defaultFood);
-    console.log('this.chosenFood', this.chosenFood);
+  activate(){
+
   }
 
+  addFood(){
+    this.entryFoodItems.push(new EntryFoodItem());
+  }
+
+  //TODO: rename to submit
   entry(){
+    var jsonEntryFoodItem = [];
+    for (var efi of this.entryFoodItems) {
+      jsonEntryFoodItem.push(efi.toJSON());
+    }
+
     var data = {
       glucoseLevel : this.glucose,
       exerciseCarbs : this.exercise,
       insulinShort : this.short,
-      foodItems : this.chosenFood //not updating from default when you select a new food
+      foodItems : jsonEntryFoodItem //not updating from default when you select a new food
     };
-
-
-    // for (let i of this.chosenFood) {
-
-    // };
-
 
     return this.client.post('/entry?add', JSON.stringify(data)).then(response => {
       
@@ -115,35 +70,68 @@ export class Entry{
     return moment().format("dddd, Do MMMM YYYY, h:mm a");
   }
 
-  plural(quantity){
-    console.log('quantity', quantity);
-
-    if (quantity > 1 || quantity === 0) {
-      console.log('yup', quantity);
-      return 's';
+  totalCarbs(){
+    total = 0;
+    for (var efi of this.entryFoodItems) {
+      total += efi.getTotal();
     }
-  }
 
-  get totalCarbs(){
-    var calculation = parseInt(this.quantity) * parseInt(this.chosenFood.carbs);
-    return `${calculation}`;
+    return total;
+    //var calculation = parseInt(this.quantity) * parseInt(this.chosenFood.carbs);
+    //return `${calculation}`;
   }
 
   get calc(){
-    var normalAdjust = (parseInt(this.glucose) - 7) / 3,
-        foodAdjust = (isNaN(parseInt(this.totalCarbs) / 10)) ? 0 : parseInt(this.totalCarbs) / 10,
+    /*var normalAdjust = (parseInt(this.glucose) - 7) / 3,
+        foodAdjust = totalCarbs() / 10,
         exerciseAdjust = parseInt(this.exercise),
         calculation = Math.round(((normalAdjust + foodAdjust - exerciseAdjust) * 10) / 10);
 
         calculation = (calculation < 0) ? 0 : calculation;
 
-    return `${calculation}`;
+    return calculation;*/
+    return 100;
+    //return `${calculation}`;
+  }
+
+}
+
+export class EntryFoodItem {
+  
+  constructor() {
+    this.id = null;
+    this.name = 'Select food item:';
+    this.unit = 'unit';
+    this.carbs = 0;
+    this.quantity = 0;
+  }
+
+  get plural() {
+    return (this.quantity == 0 || this.quantity > 1)?'s':'';
+  }
+
+  toJSON() {
+    return {
+      id : this.id,
+      name : this.name,
+      carbs : this.carbs,
+      quantity : this.quantity 
+    };
+  }
+
+  set foodItem(item) {
+    this.id = item.id;
+    this.name = item.name;
+    this.quantity = item.defaultAmount;
+    this.carbs = item.carbs;
+    this.unit = item.unit;
+  }
+
+  getTotal() {
+    return (this.carbs * this.quantity);
   }
 
 }
 
 
-//1. user inputs data, new entry record is saved on submit.
-//2. user inputs new food name, number of carbs per unit and quantity, new food record is saved on submit ("unit" can be default unit name)
-//3. api returns food list
 
