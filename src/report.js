9511cc57702ddbe93b105ea8209eb3b9143d2e6f
Rebecca Hill <r3b3ccahill@gmail.com>
2015-04-20 22:27:40 +1200

    Styling reports

--- src/report.js
+++ src/report.js
@@ -12,7 +12,33 @@ export class Report{
 
     this.client.get('/entry').then(response => {
       this.entries = response.content;
+
+      for (var entry of this.entries) {
+        entry.exerciseCarbs = Math.round(entry.exerciseCarbs);
+        entry.glucoseLevel = Math.round(entry.glucoseLevel);
+        entry.insulinShort = Math.round(entry.insulinShort);
+        entry.foodAdjust = 0;
+
+        for (var food of entry.foodItems) {
+          food.quantity = Math.round(food.quantity);
+          entry.foodAdjust += (food.quantity * food.carbs);
+        }
+
+        if (entry.glucoseLevel < 4) {
+          entry.levelKeyword = "low";
+        } else if (entry.glucoseLevel > 9) {
+          entry.levelKeyword = "high";
+        } else {
+          entry.levelKeyword = "ideal";
+        }
+
+        this.entries.splice(4, this.entries.length);
+
+        console.log('entry', entry);
+      }
     });
+
+
   }
 
 
