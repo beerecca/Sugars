0724d536fe21bf18fa285eeb3d1c5259458365d1
Ben Naylor <nayben@gmail.com>
2015-09-28 19:37:46 +1300

    binding to correct port for heroku

--- app.js
+++ app.js
@@ -2,5 +2,9 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('.'));
+app.set('port', (process.env.PORT || 5000));
 
-var server = app.listen(3000);
+app.listen(app.get('port'), function() {
+    console.log('Up and running on all cylinders');
+});
+
